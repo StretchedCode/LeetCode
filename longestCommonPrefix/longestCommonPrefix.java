c62261c62bba08eda5cec2f6255663fe39ca82fd
Ghufran <ghufranshahid101@gmail.com>
2022-10-25 22:01:41 -0400

    Implementation of finding the longest commone prefix in Java. Very Bruteforce-esque with with specific cases built in and using Arrays.sort

---
+++ longestCommonPrefix/longestCommonPrefix.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+/**
+ * longestCommonPrefix
+ */
+public class longestCommonPrefix {
+
+    public static void main(String[] args) {
+        String [] words = {"ab", "a"};
+        System.out.println(longestCommonPrefix(words));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        
+        String prefix = "";
+        char checkChar = 'z';
+        boolean breakCheck = false;
+        Arrays.sort(strs);
+
+        for (int i = 0; i < strs[0].length(); i++){
+
+            if (strs.length == 1){
+                prefix = strs[0];
+                breakCheck = true;
+            }
+
+
+            if (breakCheck){
+                break;
+            }
+
+            checkChar = strs[0].charAt(i);
+            int count = 0;
+
+            for (int j = 1; j < strs.length; j++){
+
+                if (checkChar == strs[j].charAt(i)){
+                   // System.out.println("Yes");
+                    
+                   count++;
+
+                   if (count == strs.length - 1){
+                    prefix += checkChar;
+                   }
+                }
+                else{
+                    breakCheck = true;
+                    break;
+                }
+
+            }
+        }
+
+        return prefix;
+    }
+}
